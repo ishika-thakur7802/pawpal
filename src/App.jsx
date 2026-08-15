838cb8a154ee68b75b6022b8d41ff5e3621405ca
Ishika Thakur <ishikathakur0708@gmail.com>
2026-08-15 07:23:07 +0530

    using useState to set the questions automatically on clicking the sample question buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [question, setQuestion] = useState("");
   return (
     <main>
       <h1>🐾 PawPal</h1>
@@ -11,27 +13,34 @@ function App() {
         Ask anything about your new dog and get simple, friendly guidance.
       </p>
 
-      <textarea
-        placeholder="What is your dog doing?"
-        rows="5"
-      />
+     <textarea
+       placeholder="What is your dog doing?"
+       rows="5"
+       value={question}
+       onChange={(event) => setQuestion(event.target.value)}
+     />
 
       <button>Ask PawPal 🐾</button>
 
       <div className="examples">
         <p>Try asking:</p>
 
-        <button className="example-button">
-          Why is my puppy crying?
-        </button>
+       <button
+         className="example-button"
+         onClick={() => setQuestion("Why is my puppy crying?")}
+       >
+         Why is my puppy crying?
+       </button>
 
-        <button className="example-button">
+        <button className="example-button"
+        onClick={() => setQuestion("Why won't my dog eat?")}>
           Why won't my dog eat?
         </button>
 
-        <button className="example-button">
+        <button className="example-button"
+         onClick={() => setQuestion("Why does my puppy bite me?")}>
           Why does my puppy bite me?
-        </button>
+         </button>
       </div>
     </main>
   );
